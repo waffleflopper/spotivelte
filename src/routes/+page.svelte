090721f2795aee76aaa42997ec9edc4b9a80dd95
Robert-MacMini <robertbaddeley@gmail.com>
2023-02-21 16:36:39 -0600

    normalize, scss, toRem setup, breakpoints

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	h1 {
+		color: red;
+		font-size: functions.toRem(32);
+		@include breakpoint.up('md') {
+			color: green;
+		}
+		@include breakpoint.up('lg') {
+			color: blue;
+		}
+		@include breakpoint.down('md') {
+			color: black;
+			text-decoration: line-through;
+		}
+	}
+</style>
