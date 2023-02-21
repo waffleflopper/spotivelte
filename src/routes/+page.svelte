6903afda58e6cd6c6554d53a9907f85629877d66
Robert-MacMini <robertbaddeley@gmail.com>
2023-02-21 16:57:51 -0600

    global stylings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<style lang="scss">
-	h1 {
-		color: red;
-		font-size: functions.toRem(32);
-		@include breakpoint.up('md') {
-			color: green;
-		}
-		@include breakpoint.up('lg') {
-			color: blue;
-		}
-		@include breakpoint.down('md') {
-			color: black;
-			text-decoration: line-through;
-		}
-	}
-</style>
+<input />
+<select>
+	<option>option</option>
+</select>
+<button>Button</button>
