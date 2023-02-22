497622819b59c7b443cf1dd4d62b3123b01818b1
Robert-AirM1 <robertbaddeley@gmail.com>
2023-02-22 15:56:23 -0600

    logout button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import LogoutButton from '$lib/components/LogoutButton.svelte';
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
 	import type { PageData } from './$types';
@@ -9,6 +10,7 @@
 {#if data.user}
 	<div class="top">
 		<h1>Welcome {data.user?.display_name}</h1>
+		<LogoutButton />
 	</div>
 {/if}
 <slot />
