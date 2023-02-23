5d559e1e6cc057b529d0529f91bf0ef7c3434c51
Robert-AirM1 <robertbaddeley@gmail.com>
2023-02-23 17:37:22 -0600

    assets index, new logos

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as cover } from './cover.png';
+export { default as playing } from './playing.gif';
+export { default as logo } from './spotify-logo--no-text.png';
+export { default as textLogo } from './spotify-logo--with-text.png';
