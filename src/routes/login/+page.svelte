9c469fe1d45fba09545844bf254e61cb273d2ff0
Robert-MacMini <robertbaddeley@gmail.com>
2023-02-22 21:51:26 -0600

    working on Button component w/ interface and type

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,7 @@
-<a href="/api/auth/login">Login To Spotify</a>
+<script lang="ts">
+	import Button from '$components/Button.svelte';
+</script>
+
+<Button element="button" variant="danger">Warning</Button>
+<Button element="a" href="/api/auth/login" variant="outline">Outline</Button>
+<Button element="button">Solid</Button>
